576b3a5dea7d106463937b7103e08a024bfe2bec
Miguel <miguel@mayosoftware.es>
2023-10-24 11:00:12 +0200

    Notifications permissions

--- lib/src/permissions_repository.dart
+++ lib/src/permissions_repository.dart
@@ -47,4 +47,19 @@ class PermissionsRepository {
     final status = await Permission.location.request();
     return status.isGranted;
   }
+
+  /// NOTIFICATIONS
+
+  /// Check if the user has the permission to access the notifications
+  Future<bool> isNotificationPermissionsDenied() async {
+    final status = await Permission.notification.status;
+
+    return status.isPermanentlyDenied;
+  }
+
+  /// Request the permission to access the notifications
+  Future<bool> requestNotificationPermissions() async {
+    final status = await Permission.notification.request();
+    return status.isGranted;
+  }
 }
